ce43573029f763caa88634e9fb9aefa994cb1544
i3nriq <kikemaxmq@hotmail.com>
2022-11-14 20:00:59 +0100

    3 files modified

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ import './style.css';
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+//Gráfico de columnas. Mi 1º gráfico
+var visualization = d3plus.viz()
+ .container("#viz1")
+ .data([{"year": 1991, "name":"alpha", "value": 15},
+ {"year": 1992, "name":"alpha", "value": 20},])
+ .type("bar")
+ .id("name")
+ .x("year")
+ .y("value")
+ .draw()
+
+
+ d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
+  var visualization = d3plus.viz()
+  .container("#viz2")
+  .data(data)
+  .type('bar')
+  .id('name')
+  .x('year')
+  .y('value')
+  .axes({ ticks: 'false' })
+  .draw();
+ });
